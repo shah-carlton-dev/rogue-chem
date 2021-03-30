9798901ba0dbf2850b6347dc974cd455f977fa7c
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2021-03-29 21:05:42 -0400

    look at this rushl

--- app/src/components/VideoRender/VideoView.jsx
+++ app/src/components/VideoRender/VideoView.jsx
@@ -4,11 +4,12 @@ import {Card} from 'react-bootstrap';
 const VideoView = (props) => {
     return(
         <>
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '20vw' }}>
             <Card.Body>
                 <Card.Title>{props.video.title}</Card.Title>
                 <Card.Text>
-                    {props.video.description}
+                <iframe src={props.video.url} width="640" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                <p>{props.video.description}</p>
                 </Card.Text>
             </Card.Body>
         </Card>
