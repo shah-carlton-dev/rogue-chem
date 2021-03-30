080bece700753018d46265a083233ceec55592f8
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2021-03-29 21:05:30 -0400

    another push lolololol

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,10 +20,9 @@ Router.post('/adminCreate',
                 username, password, email, fname, lname, courses, messages, admin: true
             });
             await admin.save();
-            res.send(admin).status(200);
+            return res.send(admin).status(200);
         } catch (err) {
-            res.send('Error while creating admin').status(400);
-            console.log(err);
+            return res.send('Error while creating admin').status(400);
         }
     }
 );
@@ -43,10 +42,9 @@ Router.post('/studentCreate',
                 admin: false
             });
             await student.save();
-            res.send(student).status(200);
+            return res.send(student).status(200);
         } catch (err) {
-            res.send('Error while creating student user').status(400);
-            console.log(err);
+            return res.send('Error while creating student user').status(400);
         }
     }
 );
@@ -54,16 +52,16 @@ Router.post('/studentCreate',
 Router.post('/validateToken', async (req, res) => {
     try {
         const token = req.body["auth-token"];
-        if (!token) { res.send(false).status(301); return; }
+        if (!token) { return res.send(false).status(301); return; }
 
         const verified = jwt.verify(token, constants.jwt_pass);
-        if (!verified) { res.send(false).status(302); return; }
+        if (!verified) { return res.send(false).status(302); return; }
 
         let existing;
         await Student.findById(verified.id, (err, user) => {
             if (err) {
                 console.log(err);
-                res.send("Error finding user").status(400);
+                return res.send("Error finding user").status(400);
             }
             existing = user;
         });
@@ -71,19 +69,19 @@ Router.post('/validateToken', async (req, res) => {
             await Admin.findById(verified.id, (err, user) => {
                 if (err) {
                     console.log(err);
-                    res.send("Error finding account").status(400);
+                    return res.send("Error finding account").status(400);
                 }
                 existing = user;
             });
-            if (existing == null) { res.send(false).status(303); return; }
+            if (existing == null) { return res.send(false).status(303); }
         }
-        res.send({
+        return res.send({
             valid: true,
             token: token,
             user: existing
         }).status(200);
     } catch (err) {
-        console.log(err);
+        return res.send("Could not validate token").status(304);
     }
 })
 
@@ -94,7 +92,7 @@ Router.post('/login', async (req, res) => {
         await Student.findOne({ username }, (err, user) => {
             if (err) {
                 console.log(err);
-                res.send("Error finding user").status(400);
+                return res.send("Error finding user").status(400);
             }
             existing = user;
         });
@@ -102,23 +100,23 @@ Router.post('/login', async (req, res) => {
             await Admin.findOne({ username }, (err, user) => {
                 if (err) {
                     console.log(err);
-                    res.send("Error finding account").status(401);
+                    return res.send("Error finding account").status(400);
                 }
                 existing = user;
             });
         }
-        const pass = await bcrypt.compare(password, existing.password);
-        if (!pass) {
-            res.send("Invalid password").status(400);
+        let pass;
+        try {
+            pass = await bcrypt.compare(password, existing.password).then(a => null, (a) => console.log("poo"));
+        } catch (e) {
+            return res.send('Invalid password. Nice try, bot.').status(400);
         }
         const token = jwt.sign({ id: existing._id }, constants.jwt_pass);
-        res.send({ existing, token }).status(200);
+        return res.send({ existing, token }).status(200);
     }
     catch (err) {
-        res.status(400).send('Error logging in');
-        console.log(err);
+        return res.send("Error logging in - try again").status(400);
     }
-}
-)
+});
 
 module.exports = Router;
